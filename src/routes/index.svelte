13829f771e103fb1a38eb1e06032f1dc6b97d15a
Yves Tscherry <info@feerglas.com>
2021-06-02 09:48:22 +0200

    define custom elements for lyne

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,11 @@
+<script context="module">
+
+import { defineCustomElements } from 'lyne-test/loader/';
+
+defineCustomElements();
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <lyne-cta-button label="button"></lyne-cta-button>
