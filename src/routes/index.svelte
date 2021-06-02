e10ca0a578f81c958437b32510308d42e4512ec3
Yves Tscherry <info@feerglas.com>
2021-06-02 13:48:04 +0200

    wait until import promise resolves before defining custom elements

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,11 +1,11 @@
-<script context="module">
+<script>
+import { onMount } from 'svelte';
 
-import { defineCustomElements } from 'lyne-test/loader/';
+onMount(async () => {
+  const lyne = await import(/* @vite-ignore */'lyne-test/dist/custom-elements');
 
-defineCustomElements();
-
+  lyne.defineCustomElements();
+});
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 <lyne-cta-button label="button"></lyne-cta-button>
